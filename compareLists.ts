18eedc333ab6a867e0d583fd2389f470c1414553
Vince Hetet <vince.hetet@zoro.com>
2023-05-13 21:12:50 -0500

    update compare list to log the station that have changed

--- compareLists.ts
+++ compareLists.ts
@@ -2,25 +2,27 @@
 export const compareFiles = async () => {
     const newFilePath = 'list_electric_charger_2023-05-13.json';
     const oldFilePath = 'list_electric_charger_2023-05-01.json';
-    // console.log({filePath})
-    // Use fs.access() to check if the file exists
     const newData = JSON.parse(await Deno.readTextFile(newFilePath));
     const oldData = JSON.parse(await Deno.readTextFile(oldFilePath));
-    // const stationsIdList = new Set();
-
 
     const stationsMap = new Map();
-    newData.fuel_stations.forEach(station => {
+    const stationsDiffIds = new Set();
+    newData.fuel_stations.forEach((station: any) => {
+        stationsDiffIds.add(station.id);
         stationsMap.set(station.id, {
             ...stationsMap.get(station.id),
             new: station
         });
     })
-    oldData.fuel_stations.forEach(station => {
+    oldData.fuel_stations.forEach((station: any) => {
+        stationsDiffIds.has(station.id) ? stationsDiffIds.delete(station.id) : stationsDiffIds.add(station.id)
         stationsMap.set(station.id, {
             ...stationsMap.get(station.id),
             old: station
         })
+    })
+    stationsDiffIds.forEach(id => {
+        console.log(stationsMap.get(id))
     })
 }
 
