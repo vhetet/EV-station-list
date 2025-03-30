62f4b9c367042138c1effdd356acd346ead98890
Vince Hetet <vince.hetet@zoro.com>
2025-03-30 17:04:34 -0500

    add types in compareList

--- compareLists.ts
+++ compareLists.ts
@@ -1,29 +1,49 @@
+type FuelStation = {
+    id: string;
+    name: string;
+    address: string;
+    [key: string]: any; // Add this if there are additional unknown properties
+};
 
-export const compareFiles = async () => {
+type ChargerData = {
+    fuel_stations: FuelStation[];
+};
+
+export const compareFiles = async (): Promise<void> => {
     const newFilePath = 'list_electric_charger_2023-05-13.json';
     const oldFilePath = 'list_electric_charger_2023-05-01.json';
-    const newData = JSON.parse(await Deno.readTextFile(newFilePath));
-    const oldData = JSON.parse(await Deno.readTextFile(oldFilePath));
 
-    const stationsMap = new Map();
-    const stationsDiffIds = new Set();
-    newData.fuel_stations.forEach((station: any) => {
+    // Parse JSON files with proper types
+    const newData: ChargerData = JSON.parse(await Deno.readTextFile(newFilePath));
+    const oldData: ChargerData = JSON.parse(await Deno.readTextFile(oldFilePath));
+
+    const stationsMap = new Map<string, { new?: FuelStation; old?: FuelStation }>();
+    const stationsDiffIds = new Set<string>();
+
+    // Process new data
+    newData.fuel_stations.forEach((station: FuelStation) => {
         stationsDiffIds.add(station.id);
         stationsMap.set(station.id, {
             ...stationsMap.get(station.id),
-            new: station
+            new: station,
         });
-    })
-    oldData.fuel_stations.forEach((station: any) => {
-        stationsDiffIds.has(station.id) ? stationsDiffIds.delete(station.id) : stationsDiffIds.add(station.id)
+    });
+
+    // Process old data
+    oldData.fuel_stations.forEach((station: FuelStation) => {
+        stationsDiffIds.has(station.id)
+            ? stationsDiffIds.delete(station.id)
+            : stationsDiffIds.add(station.id);
         stationsMap.set(station.id, {
             ...stationsMap.get(station.id),
-            old: station
-        })
-    })
-    stationsDiffIds.forEach(id => {
-        console.log(stationsMap.get(id))
-    })
-}
+            old: station,
+        });
+    });
+
+    // Log differences
+    stationsDiffIds.forEach((id) => {
+        console.log(stationsMap.get(id));
+    });
+};
 
 compareFiles();
